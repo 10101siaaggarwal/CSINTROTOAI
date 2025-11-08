d7ce31751fb5d267ff12ff4ef9f79eeaf789ca94
Sia Aggarwal <siaa2@uci.edu>
2025-11-08 14:51:20 -0800

    header file

--- checkers-cpp/StudentAI.cpp
+++ checkers-cpp/StudentAI.cpp
@@ -30,3 +30,64 @@ Move StudentAI::GetMove(Move move)
 
 }
 
+
+class Node{
+    // bool term
+    // unsigned int size
+    unsigned int number_of_simulations;
+    double score;
+    const Gstate * state; // current 
+    const Gmov *move; // move to come from parent to current 
+    vector<Node *> *children;
+    Node * parent;
+    queue < Gmov*> *totry; // why
+    // void backpropagate(double, )
+
+    public:
+
+
+    Node(const Gstate *state,Node *parent, const Gmov *move);
+
+    ~Node();
+    bool is_expanded() const;
+    const Gmov *get_move() const;
+    unsigned int get_size() const;
+    bool is_term() const;
+
+    void expand();
+    void rollout();
+
+    Node* select_best_child(double c) const;
+    Node* advancetree(const Gmov *mov);
+    const Gstate *get_current_state() const;
+
+    void get_stats() const;
+    double calculate_winrate(bool player1turn) const;
+
+    
+};
+
+ 
+
+class GTree{
+    Node *root;
+    int max_iter = 10000, max_seconds=60;
+
+    public:
+    GTree(Gstate *start_state, int max_iter);
+    ~GTree();
+    Node* select(double c=? ); // select child to expand
+    Node* select_best_child(); // select best 
+    
+    void grow_tree(int max_iter, double_max_time_insecs);
+
+    void advance_tree(const Gmove *move);
+    unsigned int get_size() const;
+    const Gstate *get_current_state() const;
+
+    void get_states() const;
+
+};
+
+
+class 
